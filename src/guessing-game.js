25d9de2304b2052836f4b16fc51f33742028f7fc
Anna Volkova <arollinna@gmail.com>
2019-10-06 20:58:04 +0400

    fix: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min = 0;
+        this._max = 0; 
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max; 
     }
 
     guess() {
-
+        return Math.round((this._min+this._max)/2);
     }
 
     lower() {
-
+        this._max = this.guess(); 
     }
 
     greater() {
-
+        this._min = this.guess();
     }
 }
 
